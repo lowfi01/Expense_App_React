1ce78629ef519602d72d52df6da55d4f64f76921
james herrero <jamesherrero@gmail.com>
2019-01-24 17:25:48 +1100

    setup production build and server, heroku rdy

---
+++ server/server.js
@@ -0,0 +1,17 @@
+const express = require('express');
+const path = require('path');
+
+const app = express();
+const publicPath = path.join(__dirname, '..', 'public');
+const port = process.env.PORT || 3000; // define port to work with heroku
+
+app.use(express.static(publicPath));
+
+app.get('*', (req, res) => {
+  // Allow url paths to resolve correctly
+  res.sendFile(path.join(publicPath, 'index.html'))
+})
+
+app.listen(port, ()=> {
+  console.log(`Server live on ${port}`);
+})
