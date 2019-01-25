9062aaadc2e96edd8cdc22736c887a3c696c9475
james herrero <jamesherrero@gmail.com>
2019-01-25 16:37:07 +1100

    setup dev dependencies and dist folder

--- public/scripts/app.js
+++
@@ -1,5 +0,0 @@
-'use strict';
-
-require('./utils');
-
-console.log('app.js is running');
--- webpack.config.js
+++ webpack.config.js
@@ -11,7 +11,7 @@ module.exports = (env, argv) => {
     entry : './src/app.js',
     mode: 'development',
     output : {
-      path : path.join(__dirname, 'public'),
+      path : path.join(__dirname, 'public', 'dist'),
       filename: 'bundle.js'
     },
     module : {
@@ -44,7 +44,8 @@ module.exports = (env, argv) => {
     devtool: isProduction ? 'source-map' : 'inline-source-map',
     devServer: {
       contentBase : path.join(__dirname, 'public'),
-      historyApiFallback: true
+      historyApiFallback: true,
+      publicPath: '/dist/'
     }
   }
 }
